61d192ad1e89b98b3f1adeeb34bff12c495360b2
Fire_Yw <fire15@126.com>
2017-03-09 22:35:09 +0800

    Update test.py

--- ML_in_action/Logisitc/test.py
+++ ML_in_action/Logisitc/test.py
@@ -1,5 +1,15 @@
 #coding:utf-8
 import logRegres
+from numpy import *
+
 
 dataArr,labelMat=logRegres.loadDataSet()
-print logRegres.gradAscent(dataArr,labelMat)
+#print logRegres.gradAscent(dataArr,labelMat)
+
+#weights = logRegres.gradAscent(dataArr,labelMat)
+#print weights,weights.getA()
+#logRegres.plotBestFit(weights.getA())  #矩阵通过这个getA()这个方法可以将自身返回成一个n维数组对象,
+                            #不然直接使用weights在plotBestFit函数中的weights[1]就不是一个数而是[ 0.48007329]了
+
+weights = logRegres.stocGradAscent1(array(dataArr),labelMat)
+logRegres.plotBestFit(weights)
