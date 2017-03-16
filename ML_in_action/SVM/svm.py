a431078b9976f50ed7884c6567da200cf69ab4dc
Fire_Yw <fire15@126.com>
2017-03-16 21:19:14 +0800

    Update svm.py

--- ML_in_action/SVM/svm.py
+++ ML_in_action/SVM/svm.py
@@ -11,7 +11,7 @@ def loadDataSet(fileName):    #加载数据
         labelMat.append(float(lineArr[2]))
     return dataMat,labelMat     #返回数据集和标签集
 
-def selectJrand(i,m):   #i是alpha的下标，m是所有alpha的数目 / 只要函数值不等于输入值i，函数就会进行随机选择
+def selectJrand(i,m):   #i是alpha的下标，m是所有alpha（拉格朗日乘子）的数目 / 只要函数值不等于输入值i，函数就会进行随机选择
     j=i
     while  (j==i):
         j = int(random.uniform(0,m))
@@ -27,22 +27,25 @@ def clipAlpha(aj,H,L):   #调整大于H或者小于L的alpha的值
 
 #简化版SMO算法
 def smoSimple(dataMatIn,classLabels,C,toler,maxIter):
-    dataMatrix = mat(dataMatIn); labelMat = mat(classLabels).transpose()
+    #参数：数据集、类别标签、常熟C。容错率、退出前最大循环次数。
+    dataMatrix = mat(dataMatIn); labelMat = mat(classLabels).transpose()#转置为列向量
     b = 0; m,n = shape(dataMatrix)
     alphas = mat(zeros((m,1)))
-    iter = 0
+    iter = 0  #没有任何α改变的情况下遍历数据集的次数
     while (iter < maxIter):
-        alphaPairsChanged = 0
+        alphaPairsChanged = 0 #按对改变α的值，这里记录是否更改
         for i in range(m):
-            fXi = float(multiply(alphas,labelMat).T*(dataMatrix*dataMatrix[i,:].T)) + b
-            Ei = fXi - float(labelMat[i])
+            fXi = float(multiply(alphas,labelMat).T*(dataMatrix*dataMatrix[i,:].T)) + b #由观察大胆猜测，.T是转置的意思
+            #fXi是什么意思还在大胆猜测中...
+            Ei = fXi - float(labelMat[i]) #这里相减 难道是预测标签的意思吗
             if ((labelMat[i]*Ei < -toler) and (alphas[i] < C)) or ((labelMat[i]*Ei > toler) and (alphas[i] > 0)):
-                j = selectJrand(i,m)
+                j = selectJrand(i,m)    #随机选择第二个α
                 fXj = float(multiply(alphas,labelMat).T*(dataMatrix*dataMatrix[j,:].T)) + b
                 Ej = fXj - float(labelMat[j])
-                alphaIold = alphas[i].copy();
+                alphaIold = alphas[i].copy(); #拷贝一份  记录i，j两点原始的位置，便于后面计算修改量
                 alphaJold = alphas[j].copy();
-                if (labelMat[i] != labelMat[j]):
+
+                if (labelMat[i] != labelMat[j]):  #保证α在0与C之间
                     L = max(0,alphas[j] - alphas[i])
                     H = min(C,C + alphas[j] - alphas[i])
                 else:
@@ -57,10 +60,10 @@ def smoSimple(dataMatIn,classLabels,C,toler,maxIter):
                 alphas[j] = clipAlpha(alphas[j],H,L)
                 if (abs(alphas[j] - alphaJold) < 0.00001): 
                     print "j not moving enough";continue
-                alphas[i] += labelMat[j]*labelMat[i]*(alphaJold - alphas[j])
+                alphas[i] += labelMat[j]*labelMat[i]*(alphaJold - alphas[j])  #对i进行修改，修改量与j相同但方向相反
                 b1 = b -Ei - labelMat[i]*(alphas[i]-alphaIold)*dataMatrix[i,:]*dataMatrix[i,:].T - labelMat[j]*(alphas[j] - alphaJold)*dataMatrix[i,:]*dataMatrix[j,:].T
                 b2 = b -Ej - labelMat[i]*(alphas[i]-alphaIold)*dataMatrix[i,:]*dataMatrix[j,:].T - labelMat[j]*(alphas[j] - alphaJold)*dataMatrix[j,:]*dataMatrix[j,:].T 
-                if (0 < alphas[i]) and (C > alphas[j]):
+                if (0 < alphas[i]) and (C > alphas[j]):    #设置常数项
                     b = b1  
                 elif (0 < alphas[j]) and (C > alphas[j]):
                     b = b2
