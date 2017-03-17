25191cd572278cd59352ea9854c3a31a129d7c31
Fire_Yw <fire15@126.com>
2017-03-17 19:34:34 +0800

    Update test.py

--- ML_in_action/AdaBoost/test.py
+++ ML_in_action/AdaBoost/test.py
@@ -5,7 +5,10 @@ from numpy import *
 
 datMat,classLabels=adaboost.loadSimpData()
 
-print datMat,classLabels
+#print datMat,classLabels
 
 D = mat(ones((5,1))/5)
-print adaboost.buildStump(datMat,classLabels,D)
+#print adaboost.buildStump(datMat,classLabels,D)
+
+classifierArray = adaboost.adaBoostTrainDS(datMat,classLabels,9)
+print classifierArray
