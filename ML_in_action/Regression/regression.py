19198f4ef86b6edba05af3cbca862c33f5c18ca0
Fire_Yw <fire15@126.com>
2017-03-20 20:46:01 +0800

    Update regression.py

--- ML_in_action/Regression/regression.py
+++ ML_in_action/Regression/regression.py
@@ -23,3 +23,31 @@ def standRegres(xArr,yArr):
         return
     ws = xTx.I * (xMat.T*yMat) #.T是转置的话这里.I就是逆了呗  / 这里都是用的p138的公式
     return ws #预测的参数向量
+
+
+#####局部加权线性回归函数 / 给定x空间中任意一点，计算出对应的预测值yHat
+def lwlr(testPoint,xArr,yArr,k=1.0):
+    xMat = mat(xArr); yMat = mat(yArr).T
+    m = shape(xMat)[0]
+    weights = mat(eye((m)))  #numpy.eye()创建对角矩阵
+    for j in range(m):
+        diffMat = testPoint - xMat[j,:]
+        weights[j,j] = exp(diffMat*diffMat.T/(-2.0*k**2))  #权值大小以指数级衰减，参数k控制衰减速度
+    xTx = xMat.T * (weights * xMat)
+    if linalg.det(xTx) == 0.0:  #判断行列式是否为0
+        print 'this matrix is singular, cannot do inverse'
+        return
+    ws = xTx.I * (xMat.T * (weights * yMat))   #按照书上的公式 / 得到对回归系数ws的一个估计
+    return testPoint *ws
+
+def lwlrTest(testArr,xArr,yArr,k=1.0):   #用于为数据集中每个点调用lwlr()，有助于求解k的大小
+    m = shape(testArr)[0]
+    yHat = zeros(m)
+    for i in range(m):
+        yHat[i] = lwlr(testArr[i],xArr,yArr,k)
+    return yHat
+
+#缩减系数：岭回归（在矩阵XT*X上加一个kI从而使矩阵非奇异，进而能求逆，I为单位矩阵 / 缩减法的一种，相当于对回归系数的大小施加了限制）
+#lasso：限制回归系。难以求解，可使用计算简便的逐步线性回归法（属于贪心算法，每一步都尽可能减少误差）求近似结果。
+
+#权衡方差与误差。可指出哪些特征时关键的，哪些是不重要的。
