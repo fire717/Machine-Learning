b4fb6208761579b792ac9447905de3a60b8e2471
Fire_Yw <fire15@126.com>
2017-05-14 17:11:32 +0800

    Create recommendations.py

---
+++ JTZHBC/2.recommendation/recommendations.py
@@ -0,0 +1,191 @@
+# A dictionary of movie critics and their ratings of a small
+# set of movies
+critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
+ 'Just My Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5, 
+ 'The Night Listener': 3.0},
+'Gene Seymour': {'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5, 
+ 'Just My Luck': 1.5, 'Superman Returns': 5.0, 'The Night Listener': 3.0, 
+ 'You, Me and Dupree': 3.5}, 
+'Michael Phillips': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0,
+ 'Superman Returns': 3.5, 'The Night Listener': 4.0},
+'Claudia Puig': {'Snakes on a Plane': 3.5, 'Just My Luck': 3.0,
+ 'The Night Listener': 4.5, 'Superman Returns': 4.0, 
+ 'You, Me and Dupree': 2.5},
+'Mick LaSalle': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 
+ 'Just My Luck': 2.0, 'Superman Returns': 3.0, 'The Night Listener': 3.0,
+ 'You, Me and Dupree': 2.0}, 
+'Jack Matthews': {'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0,
+ 'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
+'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}}
+
+
+from math import sqrt
+
+###先找出两个相似的用户 
+# Returns a distance-based similarity score for person1 and person2 基于欧几里得距离的相似度评价
+def sim_distance(prefs,person1,person2):
+  # Get the list of shared_items
+  si={}
+  for item in prefs[person1]: 
+    if item in prefs[person2]: si[item]=1
+
+  # if they have no ratings in common, return 0
+  if len(si)==0: return 0
+
+  # Add up the squares of all the differences
+  sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2)  #计算每个相同物品的差值
+                      for item in prefs[person1] if item in prefs[person2]]) #这行代码漂亮！
+
+  return 1/(1+sum_of_squares)  #数值越大相似度越大
+
+# Returns the Pearson correlation coefficient for p1 and p2 皮尔逊相关系数
+def sim_pearson(prefs,p1,p2):
+  # Get the list of mutually rated items
+  si={}
+  for item in prefs[p1]: 
+    if item in prefs[p2]: si[item]=1
+
+  # if they are no ratings in common, return 0
+  if len(si)==0: return 0
+
+  # Sum calculations 所有偏好求和
+  n=len(si)
+  
+  # Sums of all the preferences 求平方和
+  sum1=sum([prefs[p1][it] for it in si])
+  sum2=sum([prefs[p2][it] for it in si])
+  
+  # Sums of the squares 求乘积之和
+  sum1Sq=sum([pow(prefs[p1][it],2) for it in si])
+  sum2Sq=sum([pow(prefs[p2][it],2) for it in si])	
+  
+  # Sum of the products 计算皮尔逊评价值
+  pSum=sum([prefs[p1][it]*prefs[p2][it] for it in si])
+  
+  # Calculate r (Pearson score)
+  num=pSum-(sum1*sum2/n)
+  den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
+  if den==0: return 0
+
+  r=num/den
+
+  return r
+
+# Returns the best matches for person from the prefs dictionary. 
+# Number of results and similarity function are optional params.
+# 返回前n个与目标用户最匹配的用户
+def topMatches(prefs,person,n=5,similarity=sim_pearson):
+  scores=[(similarity(prefs,person,other),other) 
+                  for other in prefs if other!=person] #这个列表推导式用的好！
+  scores.sort()
+  scores.reverse()
+  return scores[0:n]
+
+# Gets recommendations for a person by using a weighted average
+# of every other user's rankings
+def getRecommendations(prefs,person,similarity=sim_pearson):
+  totals={}
+  simSums={}
+  for other in prefs:
+    # don't compare me to myself
+    if other==person: continue
+    sim=similarity(prefs,person,other)
+
+    # ignore scores of zero or lower
+    if sim<=0: continue
+    for item in prefs[other]:
+	    
+      # only score movies I haven't seen yet
+      if item not in prefs[person] or prefs[person][item]==0:
+        # Similarity * Score
+        totals.setdefault(item,0)
+        totals[item]+=prefs[other][item]*sim
+        # Sum of similarities
+        simSums.setdefault(item,0)
+        simSums[item]+=sim
+
+  # Create the normalized list
+  rankings=[(total/simSums[item],item) for item,total in totals.items()]
+
+  # Return the sorted list
+  rankings.sort()
+  rankings.reverse()
+  return rankings
+
+###匹配商品
+def transformPrefs(prefs):
+  result={}
+  for person in prefs:
+    for item in prefs[person]:
+      result.setdefault(item,{})
+      
+      # Flip item and person 物品和人对调
+      result[item][person]=prefs[person][item]
+  return result
+
+#基于物品的协同过滤 在大数据集下比基于用户更好
+def calculateSimilarItems(prefs,n=10):
+  # Create a dictionary of items showing which other items they
+  # are most similar to.建立字典给出与这些物品最相近的所有其他物品
+  result={}
+  # Invert the preference matrix to be item-centric 以物品为中心对偏好矩阵实施倒置处理
+  itemPrefs=transformPrefs(prefs)
+  c=0
+  for item in itemPrefs:
+    # Status updates for large datasets 针对大数据集更新状态变量
+    c+=1
+    if c%100==0: print "%d / %d" % (c,len(itemPrefs))
+    # Find the most similar items to this one
+    scores=topMatches(itemPrefs,item,n=n,similarity=sim_distance)
+    result[item]=scores
+  return result
+
+def getRecommendedItems(prefs,itemMatch,user):
+  userRatings=prefs[user]
+  scores={}
+  totalSim={}
+  # Loop over items rated by this user
+  for (item,rating) in userRatings.items( ):
+
+    # Loop over items similar to this one
+    for (similarity,item2) in itemMatch[item]:
+
+      # Ignore if this user has already rated this item
+      if item2 in userRatings: continue
+      # Weighted sum of rating times similarity
+      scores.setdefault(item2,0)
+      scores[item2]+=similarity*rating
+      # Sum of all the similarities
+      totalSim.setdefault(item2,0)
+      totalSim[item2]+=similarity
+
+  # Divide each total score by total weighting to get an average
+  rankings=[(score/totalSim[item],item) for item,score in scores.items( )]
+
+  # Return the rankings from highest to lowest
+  rankings.sort( )
+  rankings.reverse( )
+  return rankings
+
+#数据集 https://grouplens.org/datasets/movielens/
+def loadMovieLens(path='/data/movielens'):
+  # Get movie titles
+  movies={}
+  for line in open(path+'/u.item'):
+    (id,title)=line.split('|')[0:2]
+    movies[id]=title
+  
+  # Load data
+  prefs={}
+  for line in open(path+'/u.data'):
+    (user,movieid,rating,ts)=line.split('\t')
+    prefs.setdefault(user,{})
+    prefs[user][movies[movieid]]=float(rating)
+  return prefs
+
+'''
+1.大数据时基于物品比基于用户更快，但有维护用户相似度表的额外开销
+2.对稀疏数据，基于物品的更好。对于密集数据，两者效果几乎一样。
+3.基于用户的过滤方法更容易实现，且无需额外步骤，适合规模小，变化频繁的内存数据集。
+4.但某些特别应用中需要推荐用户相似的用户
+'''
