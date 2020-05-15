b40b0b1bbe9688217700d6fd83c2ca39670a402d
Fire_Yw <fire15@126.com>
2020-05-16 00:21:25 +0800

    Update classify_focal_loss.py

--- tools/keras/demo/classify_focal_loss.py
+++ tools/keras/demo/classify_focal_loss.py
@@ -7,8 +7,8 @@ gamma因子用于控制易分类样本权重
 而在分类任务中，没有背景类，且keras的fit可以直接设置class_weight，所以这里直接去掉了alpha
 
 """
-
-def Classify_focal_loss(y_true,y_pred,gamma = 2):
+# version 1
+def focal_loss(y_true,y_pred,gamma = 2):
     '''
     :param y_true: ont-hot encoding ,shape is [batch_size,nums_classes]
     :param y_pred: shape is [batch_size,nums_classes],each example defined as probability for per class
@@ -18,3 +18,11 @@ def Classify_focal_loss(y_true,y_pred,gamma = 2):
     crossEntropyLoss = -((1-y_pred)**gamma)*y_true * tf.log(y_pred)#facal loss
  
     return tf.reduce_sum(crossEntropyLoss,-1)
+
+# version 2 rec
+def focal_loss(target, output, gamma=2):
+    output /= K.sum(output, axis=-1, keepdims=True)
+    eps = K.epsilon()
+    output = K.clip(output, eps, 1. - eps)
+    return -K.sum(K.pow(1. - output, gamma) * target * K.log(output),
+                  axis=-1)
