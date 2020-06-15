8c48a1ac7122d838706dc0ef0dad040ecf44e61a
Fire_Yw <fire15@126.com>
2020-06-15 11:53:10 +0800

    Create fmeasure_metric.py

---
+++ Base/frameworks/keras/demo/fmeasure_metric.py
@@ -0,0 +1,45 @@
+import keras.backend as K
+
+def binary_accuracy(y_true, y_pred, threshold=0.5):
+    if threshold != 0.5:
+        threshold = K.cast(threshold, y_pred.dtype)
+        y_pred = K.cast(y_pred > threshold, y_pred.dtype)
+    return K.mean(K.equal(y_true, K.round(y_pred)), axis=-1)
+
+def precision(y_true, y_pred):
+    # Calculates the precision
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def recall(y_true, y_pred):
+    # Calculates the recall
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def fbeta_score(y_true, y_pred, beta=1):
+    # Calculates the F score, the weighted harmonic mean of precision and recall.
+    if beta < 0:
+        raise ValueError('The lowest choosable beta is zero (only precision).')
+    
+    # If there are no true positives, fix the F score at 0 like sklearn.
+    if K.sum(K.round(K.clip(y_true, 0, 1))) == 0:
+        return 0
+    p = precision(y_true, y_pred)
+    r = recall(y_true, y_pred)
+    bb = beta ** 2
+    fbeta_score = (1 + bb) * (p * r) / (bb * p + r + K.epsilon())
+    return fbeta_score
+
+def fmeasure(y_true, y_pred):
+    # Calculates the f-measure, the harmonic mean of precision and recall.
+    return fbeta_score(y_true, y_pred, beta=1)
+    
+    
+    
+model.compile(optimizer = 'adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy',fmeasure,recall,precision])
