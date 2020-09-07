2908d11a2f597cb9e2ba23fb33781d632ebbd986
Mr.Fire <fire15@126.com>
2020-09-07 17:57:12 +0800

    Update layer_trainable.py

--- Base/frameworks/keras/demo/layer_trainable.py
+++ Base/frameworks/keras/demo/layer_trainable.py
@@ -3,6 +3,7 @@
 model = ...
 
 
+opt = Adam(lr=float(0.001))
 for layer in model.layers[:-8]:
     layer.trainable = False
 print(model.summary())
@@ -15,9 +16,9 @@ model.fit_generator(myGenerator(train_generator,cate_names_final,pre_to_label),
     validation_data=myGenerator(val_generator,cate_names_final,pre_to_label), 
     steps_per_epoch=count_train // batch_size,
     validation_steps=count_val // batch_size,
-    epochs=cfg['epochs'],
+    epochs=6,
     class_weight='auto',
-    callbacks=[earlystop,checkpoint,reduce_lr])
+    callbacks=[reduce_lr])
 
 
 
@@ -25,6 +26,7 @@ for layer in model.layers[:-8]:
     layer.trainable = True
 print(model.summary())
 
+opt = Adam(lr=float(0.0001))
 model.compile(loss='binary_crossentropy',  
             optimizer=opt, 
             metrics=[binary_accuracy])#fmeasure
